f78a547ddf413ab4e7091a30d99e10602bbff74c
Bioniboy <bioniboystuff@zmagic.com>
2021-05-25 22:26:58 -0600

    Added AI Testing

---
+++ 3D Practice/Assets/Code/NPCMove.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour
+{
+    public List<Transform> _destination;
+
+    NavMeshAgent _navMeshAgent;
+    int targetIndex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        targetIndex = 0;
+    }
+
+
+    void Update()
+    {
+        Vector3 targetVector = _destination[targetIndex].transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (targetIndex < 2)
+            {
+                targetIndex += 1;
+            } else
+            {
+                targetIndex = 0;
+            }
+            
+        }
+    }
+}
---
+++ 3D Practice/Assets/Code/wallHide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallHide : MonoBehaviour
+{
+    private bool toggle = false;
+    void Start()
+    {
+// this.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        this.gameObject.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (toggle == true)
+            {
+                this.gameObject.SetActive(false);
+                toggle = false;
+            } else
+            {
+                this.gameObject.SetActive(true);
+                toggle = true;
+            }
+        }
+    }
+}
